95e212827cc48fee1b95ea4046abd78b7fcce253
lediff <diffen@me.com>
2022-04-11 23:06:07 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 50-lappen
+# Betting
---
+++ app.py
@@ -0,0 +1,40 @@
+from enum import unique
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'nyckel'
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(30), nullable=False)
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = StringField(validators=[InputRequired(), Length(min=8, max=30)], render_kw={"placeholder": "Password"})
+    submit = SubmitField("Register")
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+if __name__ == '__main__':
+    app.run(debug=True)
+

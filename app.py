04a58a9f51f6da92f2c84425dd99197f1e573859
lediff <diffen@me.com>
2022-04-12 21:36:24 +0200

    möe sås

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from enum import unique
 from flask import Flask, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import LoginManager, UserMixin, login_manager, login_user, login_required, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
@@ -12,6 +12,14 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'nyckel'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model, UserMixin):
@@ -58,7 +66,38 @@ def register():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
+
+
+    #    hashed_password = bcrypt.generate_password_hash(form.password.data)
+    #    existing_user = User(username=form.username.data, password=hashed_password)
+    #    User.query.filter_by(existing_user).first()
+    #    db.session.commit()
+
+    #    return redirect(url_for('login'))
+
+
     return render_template('login.html', form=form)
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+    
+@app.route('/dashboard', methods=['GET', 'POST'])
+def dashboard():
+    return render_template('dashboard.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 

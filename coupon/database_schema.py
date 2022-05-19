3ff6e252df032d9ea791131a467c714546bce517
lediff <diffen@me.com>
2022-05-19 20:48:33 +0200

    Cleanup and table creation

--- coupon/database_schema.py
+++ coupon/database_schema.py
@@ -45,3 +45,37 @@ def database_schema_setup():
         if conn is not None:
             conn.close()
 
+
+    try:
+        conn = psycopg2.connect(
+        host = os.getenv('COUPON_HOST'),
+        dbname = os.getenv('COUPON_DB_NAME'),
+        user = os.getenv('COUPON_DB_USER'),
+        password = os.getenv('COUPON_DB_PASSWORD'),
+        port = os.getenv('COUPON_DB_PORT')
+        )
+
+        cur = conn.cursor()
+
+        create_script = '''CREATE TABLE "member_coupons" (
+            "id" serial NOT NULL,
+            PRIMARY KEY ("id"),
+            "user_id" character varying NOT NULL,
+            "1" boolean NULL DEFAULT NULL,
+            "x" boolean NULL DEFAULT NULL,
+            "2" boolean NULL DEFAULT NULL)'''
+
+        cur.execute(create_script)
+        conn.commit()
+        
+
+    except Exception as error:
+        return error
+
+    finally:
+        if cur is not None:
+            cur.close()
+        
+        if conn is not None:
+            conn.close()
+
